39b468dfb0933a5d25eb8c4e2220bbd225972e62
Bin Yong <yongb@usi.ch>
2024-05-31 22:13:07 +0000

    implement pinyin2hanzi and pinyin_possiblities

--- process-mozillang-data.py
+++ process-mozillang-data.py
@@ -6,7 +6,14 @@ import socket
 import struct
 import subprocess
 import sys
-import unicodedata
+
+sys.path.append('libs/ypinyin-python')
+from ypinyin import normalize_pinyin
+
+unnormalized_letters = set()
+pinyin_possiblities = set()
+
+pinyin2hanzi = {}
 
 with open("mozillang-source/pinyin-data/pinyin.txt", "r") as f:
   while True:
@@ -20,6 +27,26 @@ with open("mozillang-source/pinyin-data/pinyin.txt", "r") as f:
       continue
     code = line[2:6]
     pinyin = line[8:].split(' ')[0]
-    pinyin = unicodedata.normalize('NFKD', pinyin)
+    pinyin = normalize_pinyin(pinyin)
+    for x in pinyin:
+      if x == ",":
+        continue
+      if x >= "a" and x <= "z":
+        continue
+      if x in u_letters:
+        continue
+      unnormalized_letters.add(x)
     pinyin = pinyin.split(',')
-    print(code, pinyin)
+    pinyin_possiblities.update(pinyin)
+    word = bytearray.fromhex(code).decode("utf-16be")
+    print(word, pinyin)
+    for yin in pinyin:
+      if yin not in pinyin2hanzi:
+        pinyin2hanzi[yin] = set((word,))
+      else:
+        pinyin2hanzi[yin].add(word)
+
+if unnormalized_letters:
+  print("WARNING: special cases found!", sorted(unnormalized_letters))
+print(pinyin_possiblities)
+print(pinyin2hanzi)
